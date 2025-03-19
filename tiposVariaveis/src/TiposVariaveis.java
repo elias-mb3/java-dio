36cbca825468155503d5424279be0ff89374b147
elias-mb3 <monteiroelias783@gmail.com>
2025-03-19 20:15:09 -0300

    feat: Tipos Primitivos

--- tiposVariaveis/src/TiposVariaveis.java
+++ tiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class TiposVariaveis {
+    double salario;
+    int idade;
+    float altura;
+
+    public static void main(String[] args) {
+        TiposVariaveis tv = new TiposVariaveis();
+        tv.salario = 1000.00;
+        tv.idade = 25; // int não pode começar com 0
+        tv.altura = 1.75f; // f para float
+
+        System.out.println("Salário: " + tv.salario);
+        System.out.println("Idade: " + tv.idade);
+        System.out.println("Altura: " + tv.altura);
     }
+    
+    int teste = 1;
+    final int TESTE = 2;
+
+
 }
